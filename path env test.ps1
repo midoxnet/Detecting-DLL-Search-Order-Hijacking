ccd3eca6f251b9257adcdc0d00f9dc5d280002dd
Povl TekstTV <35890107+povlteksttv@users.noreply.github.com>
2019-07-22 09:41:08 +0200

    Update path env test.ps1

--- path env test.ps1
+++ path env test.ps1
@@ -1,4 +1,4 @@
-﻿# Sript created by Povl TekstTV on 21. july 2019
+# Sript created by Povl TekstTV on 21. july 2019
 # The following script will go through each directory in the "PATH" System variable. 
 # For each directory it will test to see if any normal user has either modify, write or full control. 
 # This script is made with the following purpose: Does the PC have writable directories that could be 
@@ -9,14 +9,16 @@ $allPaths = $env:path
 $currentPath = "" 
 $allPaths.Split(";") | ForEach {
     $currentPath = $_
-    (get-acl $_).access | Where-Object {$_.FileSystemRights -like "Write*" -or $_.FileSystemRights -like "FullControl" -or $_.FileSystemRights -like "Modify*"} | ForEach {
-        $user = $_.IdentityReference
-        $permission = $_.FileSystemRights
+    if ($_ -ne "") {
+        (get-acl $_).access | Where-Object {$_.FileSystemRights -like "Write*" -or $_.FileSystemRights -like "FullControl" -or $_.FileSystemRights -like "Modify*"} | ForEach {
+            $user = $_.IdentityReference
+            $permission = $_.FileSystemRights
 
-        #Ignore the following users. They should have write access to some of these anyway
-        $ignoreUsers = @("NT SERVICE\TrustedInstaller", "NT AUTHORITY\SYSTEM", "BUILTIN\Administrators", "APPLICATION PACKAGE AUTHORITY\ALL APPLICATION PACKAGES")
-        if ($ignoreUsers -notcontains $user) {
-            echo "$user has write permissions on $currentPath"
-        } 
+            #Ignore the following users. They should have write access to some of these anyway
+            $ignoreUsers = @("NT SERVICE\TrustedInstaller", "NT AUTHORITY\SYSTEM", "BUILTIN\Administrator", "APPLICATION PACKAGE AUTHORITY\ALL APPLICATION PACKAGES")
+            if ($ignoreUsers -notcontains $user) {
+                echo "$user can write to $currentPath"
+            } 
+        }
     }
 }
